b6b3d1969de89ef15b66a5b3fe6b7b7d100bdefd
Zach Berwaldt <zberwaldt@gmail.com>
2018-11-02 13:34:39 -0400

    build new-generation script

--- news-generation.py
+++ news-generation.py
@@ -7,3 +7,19 @@ import os
 import sys
 import re
 import shutil
+
+publication_url = "https://relationalagents.com/"
+
+page = urlopen(publication_url)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+news_items = soup.find('section', attrs={'class': 'whats-new'}).find_all('li')
+
+for item in news_items:
+    links = item.find_all('a')
+    for link in links:
+        match = re.sub(link.text, item.text).group(0)
+        print(match)
+        mdLinkText = f'bloop'
+        linkHref = f'({link['href']})'
